21b0e01f0765164f3eece5953ff6f7129a75e68b
Rodrigo <rodrigo.calvo@viewnext.com>
2024-09-11 14:28:19 +0200

    frontend

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 
 
 export const environment = {
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://mymap-backend.onrender.com',
   mapbox_key: 'pk.eyJ1Ijoicm9kcmlnb2NjIiwiYSI6ImNtMDZpaWFrbDBvdGoya3M5djhubHNjZDIifQ.vvth3P8t3-Td1zxlRZtd1w'
 }
 
